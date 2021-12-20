0434e632d6857b87ab24cde8bfb5401cc10e4143
M2376 <87819975+M2376@users.noreply.github.com>
2021-12-20 12:25:41 -0500

    Made changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,63 @@
-import React, { Component } from "react";
+import React from "react";
+import Player from "./Components/player";
 import "./App.css";
 
-class App extends Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      playerAScore: 0,
+      winningScore: 0,
+      winner: "Player A"
+    };
   }
+
+  handlePlayerAClick = () => {
+    this.setState((state) => {
+      state.playerAScore += 1;
+      return state;
+    });
+  };
+
+  handlePlayerBClick = () => {
+    this.setState((state) => {
+      state.playerBScore += 1;
+      return state;
+    });
+  };
+
+  reset = () => {
+    this.setState({
+      playerAScore: 0,
+      winningScore: 0,
+      winner: "Player A"
+    });
+  };
 
   render() {
     return (
       <div className="App">
-        <h1>Rock, Paper, Scissor Game</h1>
+        <div className="banner">Rock Paper Scissor</div>
+        <h1 className="Winner">Player ## is in the lead!</h1>
+
+        <div className="Players">
+          <Player
+            id={0}
+            name={"A"}
+            score={this.state.playerAScore}
+            clickHandler={this.handlePlayerAClick}
+          />
+
+          <Player
+            id={0}
+            name={"B"}
+            score={this.state.playerBScore}
+            clickHandler={this.handlePlayerBClick}
+          />
+        </div>
+        <button className="ResetButton" onClick={this.reset}>
+          Reset
+        </button>
       </div>
     );
   }
